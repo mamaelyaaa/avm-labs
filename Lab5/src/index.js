b87b213b50543e83e314b89b3636cfcecc311b7a
mamaelyaaa <poopnp586@gmail.com>
2024-12-03 17:24:17 +0300

    feat: complete first chart

--- Lab5/src/index.js
+++ Lab5/src/index.js
@@ -1,17 +1,25 @@
 import { parseJSON } from "./jsonParser.js";
 import sampleData from './data.json' with { type: "json" };;
-import { solveModEuler } from "./methods/ME.js";
-import { solveRungeKutta } from "./methods/RK.js";
-import { dependencyChart } from "./charts/depChart.js";
+import { solveEuler } from "./methods/Euler.js";
+import { solveModEuler } from "./methods/ModEuler.js";
+import { solveRungeKutta } from "./methods/RungeKutt.js";
+import { dependencyChart } from "./charts/LineChart1.js";
 
 try {
-    const data = parseJSON(JSON.stringify(sampleData));
-    console.log(data);
+    const data  = parseJSON(JSON.stringify(sampleData));
+    const { coefficients, init, step, dot } = data
+
+    // График 1
+    const eulerResults = solveEuler({ coefficients, init, step, dot });
+    const eulerModResults = solveModEuler({ coefficients, init, step, dot });
+
+    const rungeKuttaResults = solveRungeKutta({ coefficients, init, step, dot });
+    const rungeKuttaCloseResults = solveRungeKutta({ coefficients, init, step: step / 10, dot});
     
-    const eulerResults = solveModEuler(data);
-    const rungeKuttaResults = solveRungeKutta(data);
-  
-    dependencyChart(eulerResults, rungeKuttaResults);
+    dependencyChart(eulerResults, eulerModResults, rungeKuttaResults, rungeKuttaCloseResults);
+    
+    
+
   } catch (error) {
     console.error("Ошибка выполнения программы:", error);
 }
